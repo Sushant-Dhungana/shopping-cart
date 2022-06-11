7afacd97e8f8ba0e12b428d7cf35b42bfb9cbb25
Sushant-Dhungana <sushantdhungana29@gmail.com>
2022-06-11 09:14:03 +0545

    redux store comitted

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,10 +4,26 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
+import {configureStore} from "@reduxjs/toolkit";
+import {Provider} from "react-redux";
+import productsReducer from './features/productSlice';
+
+
+
+const store = configureStore({
+  reducer:{
+    products: productsReducer,
+  }
+});
+
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
+  <Provider store={store}>
     <App />
+    </Provider>
   </BrowserRouter>
 );
 
