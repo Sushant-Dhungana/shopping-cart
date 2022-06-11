b70a6fc813c913addc01d5c479bb2c47625e66ea
Sushant-Dhungana <sushantdhungana29@gmail.com>
2022-06-11 08:33:55 +0545

    array of products comitted

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,20 @@
 const express = require("express");
 const cors = require("cors");
 
+const products = require("./products");
+
 const app = express()
 
 app.use(express.json())
+app.use(cors()) //allows us to access nodejs api
+app.get("/", (req, res) => {
+    res.send("welcome to my shop api");
+});
+app.get("/products", (req, res) => {
+    res.send(products);
+});
+
+
+const port = process.env.PORT || 8000
+
+app.listen(port, console.log(`Server running on port ${port}`));
---
+++ backend/products.js
@@ -0,0 +1,28 @@
+const products = [
+  {
+    id: 1,
+    name: "Crf",
+    desc: "best in the segment",
+    price: "NPR 1000000",
+    image:
+      "https://res.cloudinary.com/daekpsyr5/image/upload/v1654915587/shopping%20cart/crf_eum6c2.jpg",
+  },
+  {
+    id: 2,
+    name: " KTM Duke",
+    desc: "beast in the segment",
+    price: "NPR 700000",
+    image:
+      "https://res.cloudinary.com/daekpsyr5/image/upload/v1654915584/shopping%20cart/duke_zreyf4.jpg",
+  },
+  {
+    id: 3,
+    name: "Royal enfield",
+    desc: "classic at its own",
+    price: "NPR 699999",
+    image:
+      "https://res.cloudinary.com/daekpsyr5/image/upload/v1654915582/shopping%20cart/royal_enfield_jvuovh.jpg",
+  },
+];
+
+module.exports = products;
