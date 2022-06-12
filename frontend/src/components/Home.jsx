be03e5076be93ab25ea510c042d8804c975281a3
Sushant-Dhungana <sushantdhungana29@gmail.com>
2022-06-12 16:06:46 +0545

    fetch data using rtk query and map into component

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,9 +1,35 @@
-import React from 'react';
-import { useGetAllProductsQuery } from '../features/productsApi';
+// import { createAsyncThunk } from '@reduxjs/toolkit';
+import React from "react";
+// import { useSelector } from "react-redux";
+import { useGetAllProductsQuery } from "../features/productsApi";
 
 export const Home = () => {
-  const {data, error, isLoading} = useGetAllProductsQuery();
+  // const [items,status] = useSelector(state => state.products); way of getting data in createAsyncThunk
+  const { data, error, isLoading } = useGetAllProductsQuery();
   return (
-    <div>Home</div>
-  )
-}
+    <div className="home-container">
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : error ? (
+        <p>An error occured{error.data}</p>
+      ) : (
+        <>
+          <h2>New Arrivals</h2>
+          <div className="products">
+            {data?.map((product) => (
+              <div key={product.id} className="product">
+                <h3>{product.name}</h3>
+                <img src={product.image} alt={product.name} />
+                <div className="details">
+                  <span>{product.desc}</span>
+                  <span>{product.price}</span>
+                </div>
+                <button>Add to cart</button>
+              </div>
+            ))}
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
